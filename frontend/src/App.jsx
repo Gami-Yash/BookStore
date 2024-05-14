3138149c47eead921208e956a949e7d6d54049fd
Gami-Yash <gamiyash0207@gmail.com>
2024-05-14 11:21:15 +0530

    Added single book detail view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,14 @@ import Home from './pages/homePage';
 import createBook from "./pages/createBook";
 import editBook from "./pages/editBook";
 import deleteBook from "./pages/deleteBook";
-import showBook from "./pages/showBook";
+import ShowBook from "./pages/showBook";
 
 const App = () =>{
   return(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/books/create" element={<createBook />} />
-      <Route path="/books/detail/id" element={ <showBook/> } />
+      <Route path="/books/detail/:id" element={ <ShowBook/> } />
       <Route path="/books/edit/:id" element={ <editBook/>} />
       <Route path="/books/delete/:id" element={ <deleteBook/>} />
     </Routes>
